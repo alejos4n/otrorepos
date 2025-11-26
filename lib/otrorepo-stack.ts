136f3436ae6ca353f2ff91ea5cc7aa3c34eacb69
alejos4n <alejo-lopera@hotmail.com>
2025-11-26 18:31:12 -0500

    ramas

--- lib/otrorepo-stack.ts
+++ lib/otrorepo-stack.ts
@@ -12,7 +12,7 @@ export class OtrorepoStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('alejos4n/otrorepos', 'main'), //Remember to change 
+        input: CodePipelineSource.gitHub('alejos4n/otrorepos', 'master'), //Remember to change 
         commands: ['npm ci', 
                    'npm run build', 
                    'npx cdk synth']
