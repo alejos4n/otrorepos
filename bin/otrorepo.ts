b927a0f8020b880673952988c8c0b22e0cb4c4fd
alejos4n <alejo-lopera@hotmail.com>
2025-12-01 22:30:39 -0500

    agrego variables

--- bin/otrorepo.ts
+++ bin/otrorepo.ts
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
+import datos from "../utils/datos.json"
 import { OtrorepoStack } from '../lib/otrorepo-stack';
 
 const app = new cdk.App();
 new OtrorepoStack(app, 'OtrorepoStack', {
   env: {
-    account: '210615636037',
-    region: 'us-east-2',
+    account: datos.accountId,
+    region: datos.region,
   }
 });
 
